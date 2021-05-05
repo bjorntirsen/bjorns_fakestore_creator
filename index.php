048fde23c1d066e801d3906a149fc5a056bb5b11
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-05 21:44:44 +0200

    💅Style landing page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once 'App.php';
 $grouped_array = App::getGroupedArray();
 ?>
@@ -9,7 +9,7 @@ $grouped_array = App::getGroupedArray();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Björn's Fakestore!</title>
+        <title>Björn's Fakestore Creator!</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -19,34 +19,20 @@ $grouped_array = App::getGroupedArray();
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/styles2.css" rel="stylesheet" />
     </head>
     <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-xl bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Björn's Fakestore!</a>
-                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#page-top">Home</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#womensclothing">LEGO Creator</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#mensclothing">LEGO Technic</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#jewelery">LEGO Mindstorms</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <!-- Masthead-->
-        <header class="masthead bg-primary text-white text-center">
+        <main  class="masthead2 bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
+                <div class="col-lg-4 col-6 pt-5 d-flex align-items-center flex-column">
+                    <img class="masthead-avatar mb-3" src="assets/img/avataaars.svg" alt="" />
+                </div>
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
+                
                 <!-- Masthead Heading-->
                 <p class="masthead-subheading font-weight-light mb-0">Welcome to</p>
-                <h1 class="masthead-heading text-uppercase mb-0">Björn's Fakestore!</h1>
+                <h1 class="masthead-heading text-uppercase mb-0">Björn's Fakestore Creator!</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -54,98 +40,15 @@ $grouped_array = App::getGroupedArray();
                     <div class="divider-custom-line"></div>
                 </div>
             </div>
-        </header>
-        <!-- Selection Section-->
-        <section class="page-section portfolio" id="portfolio">
-            <div class="container">
-                <!-- Selection Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-5">Shop for:</h2>
-                <!-- Selection Grid Items-->
-                <div class="row justify-content-center">
-                    <!-- Item 1-->
-                    <a href="#womensclothing" class="col-md-6 col-lg-4 mb-5 js-scroll-trigger">
-                        <div class="portfolio-item mx-auto">
-                            <h3 class="text-center text-uppercase text-secondary">LEGO Creator</h3>
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img class="limit-img img-fluid" src="https://www.pricerunner.se/product/640x640/3000782206/Lego-Creator-Colosseum-10276.jpg" alt="BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats" />
-                        </div>
-                    </a>
-                    <!-- Item 2-->
-                    <a href="#mensclothing" class="col-md-6 col-lg-4 mb-5 js-scroll-trigger">
-                        <div class="portfolio-item mx-auto">
-                            <h3 class="text-center text-uppercase text-secondary">LEGO Technic</h3>
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img class="limit-img img-fluid" src="https://www.pricerunner.se/product/640x640/1908396539/Lego-Technic-Liebherr-R-9800-Graevmaskin-42100.jpg" alt="Fjallraven - Foldsack No. 1 Backpack" />
-                        </div>
-                    </a>
-                    <!-- Item 3-->
-                    <a href="#jewelery" class="col-md-6 col-lg-4 mb-5 js-scroll-trigger">
-                        <div class="portfolio-item mx-auto">
-                            <h3 class="text-center text-uppercase text-secondary">LEGO Mindstorms</h3>
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img class="limit-img img-fluid" src="https://www.pricerunner.se/product/640x640/3000646177/Lego-Mindstorms-Robot-Inventor-51515.jpg" alt="Gold & Silver Dragon" />
-                        </div>
-                    </a>
-                </div>
+            <div id="mainContainer" class="container d-flex align-items-center flex-column">
+                <form action="#">
+                    <label class="text-secondary" for="url">Create a new store:</label>
+                    <input type="text" id="url" name="url" class="text-center form-control" required placeholder="API URL here">
+                    <input type="submit" class="form-control mt-2 btn btn-outline-secondary" value="Create New Store">
+                </form>
+                <p class="mt-2">Or visit one below:</p>
             </div>
-        </section>
-        <!-- Women's clothing Section -->
-        <section class="page-section bg-primary mb-0" id="womensclothing">
-            <div class="container">
-                <!-- Women's clothing Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-white">LEGO Creator</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Women's clothing Section Content-->
-                <div class="text-black row d-flex justify-content-center">
-                    <?php App::renderProductCards($grouped_array['creator']); ?>
-                </div>
-            </div>
-        </section>
-        <!-- Men's clothing Section -->
-        <section class="page-section" id="mensclothing">
-            <div class="container">
-                <!-- Men's clothing Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">LEGO Technic</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Men's clothing Section Content-->
-                <div class="text-black row d-flex justify-content-center">
-                    <?php App::renderProductCards($grouped_array['technic']); ?>
-                </div>
-            </div>
-        </section>
-        <!-- Jewelery Section -->
-        <section class="page-section jewelery-section" id="jewelery">
-            <div class="container">
-                <!-- Jewelery Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-white">LEGO Mindstorms</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Jewelery Section Content-->
-                <div class="text-black row d-flex justify-content-center">
-                    <?php App::renderProductCards($grouped_array['mindstorms']); ?>
-                </div>
-            </div>
-        </section>
+        </main>
         <!-- Copyright Section-->
         <div class="copyright py-4 text-center text-white">
             <div class="container"><small>Copyright © Björn Tirsén 2021</small></div>
@@ -159,10 +62,12 @@ $grouped_array = App::getGroupedArray();
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Third party plugin JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        <!-- Contact form JS-->
-        <script src="assets/mail/jqBootstrapValidation.js"></script>
-        <script src="assets/mail/contact_me.js"></script>
         <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+        <!-- <script src="js/scripts.js"></script> -->
+        <!-- My added JS-->
+        <script src="js/main.js"></script>
+        <script src="js/Interface.js"></script>
+        <script src="js/StoreList.js"></script>
+        <script src="js/Store.js"></script>
     </body>
 </html>
